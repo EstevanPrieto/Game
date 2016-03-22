30d62ba54e8bf896f2e7e407a2aa35c2c8cf9b41
EstevanPrieto <estevanprieto6@gmail.com>
2016-03-21 17:06:00 -0700

    Rooms and Fixes
    I began to add some of the room nodes of the world map. However, I have
    not added their descriptions yet, since the story is still being
    planned. Also, aside from that, I fixed an issue in which the program
    did not let you to quiet when you typed the specified commands.

--- CS10-Estevan-Prieto-THEGAME.py
+++ CS10-Estevan-Prieto-THEGAME.py
@@ -1,9 +1,9 @@
 import sys 
-
+#Sets up aspects of the rooms
 node = None
 class Room:
     def __init__(self, name, description, n_path, e_path, s_path, w_path):
-        '''Etner in the room name, room description, and rooms you can access from directions as follows: north, east, south, west
+        '''Enter in the room name, room description, and rooms you can access from directions as follows: north, east, south, west
         '''
         self.name = name
         self.description = description
@@ -11,19 +11,35 @@ class Room:
         self.east = e_path
         self.south = s_path
         self.west = w_path
-        
+
+#Makes you able to move to each room       
     def move (self, direction):
-        if direction in ['q', 'quit', 'exit']:
-            sys.exit(0)
         while True:
             global node
             node = globals()[getattr(self,direction)]
             break
 
+#Rooms in the game
+start = Room('Camp Center', 'TBA', 'n_exit', 'lab', 's_exit', 'storage')
+s_exit = Room('South Exit', 'TBA', 'start', None, 'mount', None)
+lab = Room('Laboratory', 'TBA', None, 'e_exit', 'arm', 'start')
+arm = Room('Armory', 'TBA', 'lab', None, None, None)
+e_exit = Room('East Exit', 'TBA', None, 'forest', None, 'lab')
+n_exit = Room('North Exit', 'TBA', 'highway', None, 'start', None)
+storage = Room('Pantry', 'TBA', 'farm', 'start', None, 'w_exit')
+farm = Room('Farm', 'TBA', None, None, 'storage', None) 
+w_exit = Room('West Exit', 'TBA', None, 'storage', None, 'area_one')
+mount = Room('Mountain Base', 'TBA', 's_exit', None, 'slope', None)
+slope = Room('Slopes', 'TBA', 'mount', 'cliff', 'peak', None)
+cliff = Room('Cliff', 'TBA', None, None, None, 'slope')
+peak = Room('Peak', 'TBA', 'slope', 'e_peak', None, 'w_peak')
+e_peak = Room('East Peak', 'TBA', None, None, None, 'peak')
+w_peak = Room('West Peak', 'TBA', None, 'peak', None, None)
 
+node = start #sets your location to the starting room in the game
 
-while True:
-    
+#prints where you are and checks if you can move that way; prompts you for where to go or if you want to quit
+while True:    
     print node.name
     print node.description
     command = raw_input('> ')
@@ -32,6 +48,8 @@ while True:
             node.move(command)
         except:
             print 'You can\'t go that way.'
+    if command in ['q', 'quit', 'exit']:
+        sys.exit(0)
 
     
 
